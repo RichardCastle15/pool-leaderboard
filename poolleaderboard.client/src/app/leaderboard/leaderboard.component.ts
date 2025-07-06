41a7274e1d14f18864b69a05e4ac4eb4800e677b
Richard Castle <richardcastle324@yahoo.com>
2025-07-06 22:00:46 +0100

    badge for number of people selected in killer cursor: pointer for clickable things

--- poolleaderboard.client/src/app/leaderboard/leaderboard.component.ts
+++ poolleaderboard.client/src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, input, signal } from '@angular/core';
-import { NbActionsModule, NbCardModule, NbIconModule, NbTreeGridModule } from '@nebular/theme';
+import { NbActionsModule, NbBadgeModule, NbCardModule, NbIconModule, NbTreeGridModule } from '@nebular/theme';
 import { LeaderboardEntryRow } from './leaderboard-entry-row.model';
 import { TreeNode } from './tree-node.model';
 
@@ -7,7 +7,7 @@ import { TreeNode } from './tree-node.model';
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss',
-  imports: [NbTreeGridModule, NbCardModule, NbActionsModule, NbIconModule]
+  imports: [NbTreeGridModule, NbCardModule, NbActionsModule, NbIconModule, NbBadgeModule]
 })
 export class LeaderboardComponent {
   entries = input<TreeNode<LeaderboardEntryRow>[]>([]);
