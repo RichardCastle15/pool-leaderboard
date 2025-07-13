bfd5b3ed8c477fb80eb17b9d7998fafc94f009e8
Richard Castle <richardcastle324@yahoo.com>
2025-07-13 18:13:57 +0100

    start testing leaderboard

--- poolleaderboard.client/src/app/leaderboard/leaderboard.component.spec.ts
+++ poolleaderboard.client/src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,14 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LeaderboardComponent } from './leaderboard.component';
+import { DOCUMENT } from '@angular/common';
+import { NbIconModule, NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { By } from '@angular/platform-browser';
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent;
   let fixture: ComponentFixture<LeaderboardComponent>;
 
+  const entries = [
+    {
+      data: { id: 1, name: 'Richard', points: 1906, rank: 1 },
+      children: [
+        { data: { name: 'Head-To-Head', points: 1696, rank: 2 } },
+        { data: { name: 'Killer', points: 210, rank: 1 } },
+      ]
+    },
+    {
+      data: { id: 2, name: 'Russel', points: 1711, rank: 2 },
+      children: [
+        { data: { name: 'Head-To-Head', points: 1701, rank: 1 } },
+        { data: { name: 'Killer', points: 10, rank: 4 } },
+      ]
+    },
+    {
+      data: { id: 3, name: 'Stephen B', points: 1440, rank: 3 },
+      children: [
+        { data: { name: 'Head-To-Head', points: 1400, rank: 3 } },
+        { data: { name: 'Killer', points: 40, rank: 3 } },
+      ]
+    },
+  ]
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LeaderboardComponent]
+      declarations: [],
+      imports: [LeaderboardComponent, NbThemeModule.forRoot(), NbIconModule, NbEvaIconsModule],
+      providers: [{ provide: DOCUMENT, useValue: document }]
     })
     .compileComponents();
 
@@ -19,5 +49,42 @@ describe('LeaderboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a row for each entry', () => {
+    fixture.componentRef.setInput('entries', entries);
+    fixture.detectChanges();
+    const rowCount = fixture.debugElement.queryAll(By.css('table tr')).length;
+    // 3 entries plus the header row.
+    expect(rowCount).toBe(4);
+  });
+
+  it('should highlight rows when clicked', () => {
+    fixture.componentRef.setInput('entries', entries);
+    fixture.detectChanges();
+    fixture.debugElement.queryAll(By.css('table tr')).forEach(de => de.nativeElement.click());
+    fixture.detectChanges();
+    // 3 entries plus the header row.
+    expect(fixture.debugElement.queryAll(By.css('.selected-row')).length).toBe(3);
+  });
+
+  it('should un-highlight rows when clicked again', () => {
+    fixture.componentRef.setInput('entries', entries);
+    fixture.detectChanges();
+    fixture.debugElement.queryAll(By.css('table tr')).forEach(de => de.nativeElement.click());
+    fixture.detectChanges();
+    fixture.debugElement.queryAll(By.css('table tr')).forEach(de => de.nativeElement.click());
+    fixture.detectChanges();
+    // 3 entries plus the header row.
+    expect(fixture.debugElement.queryAll(By.css('.selected-row')).length).toBe(0);
+  });
+
+  describe('action buttons', () => {
+    it('should disable head-to-head result when noone selected', () => {
+      fixture.componentRef.setInput('entries', entries);
+      fixture.detectChanges();
+      const isDisabled = fixture.debugElement.query(By.css('#head-to-head-action')).componentInstance.disabled;
+      expect(isDisabled).toBeTrue();
+    });
   });
 });
