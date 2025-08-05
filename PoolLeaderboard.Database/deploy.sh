ec7c11307559e19b85b5561c6f171b0ec5d79ae6
Richard Castle <richardcastle324@yahoo.com>
2025-08-05 13:25:52 +0000

    deploy.sh

---
+++ PoolLeaderboard.Database/deploy.sh
@@ -0,0 +1,9 @@
+# Set working directory to the script's location
+cd "$(dirname "$0")"
+
+echo 'Building...'
+dotnet build
+
+echo 'Deploying...'
+sqlpackage /Action:Publish /SourceFile:"bin/Debug/PoolLeaderboard.Database.dacpac" \
+    /TargetConnectionString:'Server=db,1433;Database=leaderboard;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;'
