814c25ffd140e94bb284b5b6764a45a973ee1ddc
Richard Castle <richardcastle324@yahoo.com>
2025-08-29 11:17:35 +0000

    killer table

--- poolleaderboard.client/src/app/killer/killer.component.ts
+++ poolleaderboard.client/src/app/killer/killer.component.ts
@@ -1,12 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, Signal } from '@angular/core';
 import { KillerGame } from './types/killer-game.model';
+import { TreeNode } from '../leaderboard/tree-node.model';
+import { KillerGameRow } from './types/killer-game-row.model';
+import { NbTreeGridModule } from "@nebular/theme";
 
 @Component({
   selector: 'app-killer',
-  imports: [],
+  imports: [NbTreeGridModule],
   templateUrl: './killer.component.html',
   styleUrl: './killer.component.scss'
 })
 export class KillerComponent {
+  readonly columns = ['name', 'livesRemaining'];
+
   game = input<KillerGame>();
+
+  tableData: Signal<TreeNode<KillerGameRow>[] | undefined>;
+
+  constructor() {
+    this.tableData = computed(() => {
+      const gameData = this.game();
+      if (!gameData)
+        return undefined;
+      return gameData.playerRows.map(pr => ({ data: pr }));
+    })
+  }
 }
