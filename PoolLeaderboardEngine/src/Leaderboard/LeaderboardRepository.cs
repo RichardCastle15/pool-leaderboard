f854681dbb40f62f4c36a5ebbd09f7bb3d2f49e9
Richard Castle <richardcastle324@yahoo.com>
2026-03-22 14:04:20 +0000

    leaderboard repo namespace

--- PoolLeaderboardEngine/src/Leaderboard/LeaderboardRepository.cs
+++ PoolLeaderboardEngine/src/Leaderboard/LeaderboardRepository.cs
@@ -1,41 +1,40 @@
-namespace PoolLeaderboardEngine.Leaderboard
+namespace PoolLeaderboardEngine.Leaderboard;
+
+public class LeaderboardRepository : ILeaderboardRepository
 {
-    public class LeaderboardRepository : ILeaderboardRepository
+    private readonly IDbConnectionFactory dbConnectionFactory;
+
+    public LeaderboardRepository(IDbConnectionFactory dbConnectionFactory)
     {
-        private readonly IDbConnectionFactory dbConnectionFactory;
+        this.dbConnectionFactory = dbConnectionFactory;
+    }
 
-        public LeaderboardRepository(IDbConnectionFactory dbConnectionFactory)
-        {
-            this.dbConnectionFactory = dbConnectionFactory;
-        }
+    public void Add(string name)
+    {
+        using var connection = dbConnectionFactory.CreateConnection();
+        connection.Open();
+        using var command = connection.CreateCommand();
+        command.CommandText = $"insert into rating (name, rating) values ('{name}', 1000)";
+        command.ExecuteNonQuery();
+    }
 
-        public void Add(string name)
+    public List<LeaderboardEntry> GetAll()
+    {
+        var entries = new List<LeaderboardEntry>();
+        using var connection = dbConnectionFactory.CreateConnection();
+        connection.Open();
+        using var command = connection.CreateCommand();
+        command.CommandText = "select * from rating";
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
         {
-            using var connection = dbConnectionFactory.CreateConnection();
-            connection.Open();
-            using var command = connection.CreateCommand();
-            command.CommandText = $"insert into rating (name, rating) values ('{name}', 1000)";
-            command.ExecuteNonQuery();
-        }
-
-        public List<LeaderboardEntry> GetAll()
-        {
-            var entries = new List<LeaderboardEntry>();
-            using var connection = dbConnectionFactory.CreateConnection();
-            connection.Open();
-            using var command = connection.CreateCommand();
-            command.CommandText = "select * from rating";
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            entries.Add(new LeaderboardEntry
             {
-                entries.Add(new LeaderboardEntry
-                {
-                    Name = (string)reader["name"],
-                    Rating = (short)reader["rating"],
-                    Id = (int)reader["id"]
-                });
-            }
-            return entries;
+                Name = (string)reader["name"],
+                Rating = (short)reader["rating"],
+                Id = (int)reader["id"]
+            });
         }
+        return entries;
     }
 }
